Fix the parallel calculation issue
The creator has been set in global scope of twofreqrfgun.py so that every worker of SCOOP could have the same creator.

When paralleled, the simulation folder for every individual must be different, we employ a simple method to resolve this.

gaoptimizer.py
@@ -95,8 +95,10 @@ class NSGAII:
             raise OptimizerError('population size has to be a multiple of 4!')
         random.seed(seed)
         toolbox = self.toolbox
-
-        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        
+        def ind_fitness(ind):
+            return ind.fitness.values
+        stats = tools.Statistics(ind_fitness)
         stats.register("avg", np.mean, axis=0)
         stats.register("std", np.std, axis=0)
         stats.register("min", np.min, axis=0)
@@ -123,7 +125,8 @@ class NSGAII:
 
             # Evaluate the individuals with an invalid fitness
             invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-            fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+            sims = ['t{0:03d}'.format(i+1) for i in range(len(invalid_ind))]
+            fitnesses = toolbox.map(toolbox.evaluate, invalid_ind, sims)
             with tqdm(total=len(invalid_ind), desc='Population', ascii=True) as pbar:
                 for ind, fit in zip(invalid_ind, fitnesses):
                     pbar.update(1)
@@ -210,7 +213,9 @@ class SPEA2:
         random.seed(seed)
         toolbox = self.toolbox
 
-        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        def ind_fitness(ind):
+            return ind.fitness.values
+        stats = tools.Statistics(ind_fitness)
         stats.register("avg", np.mean, axis=0)
         stats.register("std", np.std, axis=0)
         stats.register("min", np.min, axis=0)
@@ -226,7 +231,8 @@ class SPEA2:
         for gen in tqdm(range(ngen), leave=True, ascii=True):
             # Step 2 Fitness assignment
             invalid_ind = [ind for ind in pop if not ind.fitness.valid]
-            fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+            sims = ['t{0:03d}'.format(i+1) for i in range(len(invalid_ind))]
+            fitnesses = toolbox.map(toolbox.evaluate, invalid_ind, sims)
             with tqdm(total=len(invalid_ind), desc='Population', ascii=True) as pbar:
                 for ind, fit in zip(invalid_ind, fitnesses):
                     pbar.update(1)

twofreqrfgun.py
@@ -57,23 +57,22 @@ def gen_patch(x):
     return patch
 
 
-def evaluate(x):
-    sim = 'test2'
+def evaluate(x, sim):
     patch = gen_patch(x)
     core.run(patch, sim)
     data = core.get_data(sim, -1, 'g')
     return [nemit_t(data)[0], current_r(data), skewness(data)]
 
 
-if __name__ == '__main__':
-    # Optimizer setup
-    opt = NSGAII(evaluate)
-    opt.NDIM = 8
-    opt.OBJ = (-1, 1, -1)
-    opt.setup()
+# Optimizer setup
+opt = NSGAII(evaluate)
+opt.NDIM = 8
+opt.OBJ = (-1, 1, -1)
+opt.setup()
 
+if __name__ == '__main__':
     # Let’s rock!
-    opt.evolve(12, 2)
+    opt.evolve(16, 2)
 
     # Post-process
     xyz = np.array([ind.fitness.values for ind in opt.pop])